21666d33f9b4283011504b7ec10636e3a5f70ae5
binod164 <binod7_grg@hotmail.com>
2022-01-29 12:55:08 -0600

    Started guess-the-number-lab

--- app.js
+++ app.js
@@ -6,5 +6,30 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+
+      getGuess();
   }
 }
+game.prevGuesses = [];
+
+game.getGuess = function(){
+  let guess;
+  console.log( `Enter a guess between ${smallestNum} and ${biggestNum}`);
+  // if(typeof newGuparseInt(guess) !== 'number'){
+  //   return NaN;
+  // }else{
+  //   while(newGuess>=smallestNum && newGuess<=biggestNum){
+  //     if(newGuess === secretNum){
+  //       return `Congrats! You guessed the number in ${prevGuesses}!`
+  //     }else{
+  //       if(newGuess>secretNum){
+  //         return `Your guess is too high. Previous guesses `
+  //       }else{
+  //       return `Your guess is too low. Previous guesses `
+  //       }
+  //     }
+  //   }
+  // }
+  
+}
+console.log(game);
