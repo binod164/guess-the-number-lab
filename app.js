b059dc8d19d1b4b56abb158824a64fc879b86e30
binod164 <binod7_grg@hotmail.com>
2022-01-30 11:14:35 -0600

    Started the lab

--- app.js
+++ app.js
@@ -6,12 +6,40 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      console.log(this.secretNum);
+      // this.getGuess();
+      
   }
 }
 game.prevGuesses = [];
 
 game.getGuess = function(){
   
-  prompt( `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`);
- 
+  
+  // }
 }
+
+
+
+
+  // if(typeof parseInt(guess) !== 'number'){
+  //   return NaN;
+  // }else{
+  //   while(guess>=this.smallestNum && guess<=this.biggestNum){
+  //     if(guess === secretNum){
+  //       alert(`Congrats! You guessed the number in ${this.prevGuesses}!`)
+  //     }else{
+  //       if(guess>this.secretNum){
+  //         alert(`Your guess is too high. Previous guesses`)
+  //       }else{
+  //       alert(`Your guess is too low. Previous guesses`)
+  //       }
+      // }
+  //   }
+  // }
+  
+
+
+
+game.play();
+// console.log(this.prevGuesses);
