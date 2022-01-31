e89b745fb469ad2566f2c03580a6f98fdd6e3b1a
binod164 <binod7_grg@hotmail.com>
2022-01-30 20:32:56 -0600

    Added new functions

--- app.js
+++ app.js
@@ -6,40 +6,38 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      console.log(this.secretNum);
-      // this.getGuess();
       
+      console.log(this.secretNum);
+      while(this.prevGuesses.length === 0 || this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum){
+        this.prevGuesses.push(this.getGuess());
+        this.render();
+        
+      }
+
+      return; 
   }
 }
 game.prevGuesses = [];
 
 game.getGuess = function(){
-  
-  
-  // }
+  let guess = parseInt(prompt( `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+  while(isNaN(guess)===true || guess>this.biggestNum || guess<this.smallesttNum){ 
+    guess = parseInt(prompt( `Re - Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+  }
+  return guess;
 }
 
+game.render = function(){
+  if(this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum){
+        alert(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
+       } else{
+          if(this.prevGuesses[this.prevGuesses.length - 1]>this.secretNum){
+            alert(`Your guess is too high. Previous guesses:${this.prevGuesses.join()}`)
+          }else{
+            alert(`Your guess is too low. Previous guesses: ${this.prevGuesses.join()}`)
+            
+         }
+        }
+}
 
-
-
-  // if(typeof parseInt(guess) !== 'number'){
-  //   return NaN;
-  // }else{
-  //   while(guess>=this.smallestNum && guess<=this.biggestNum){
-  //     if(guess === secretNum){
-  //       alert(`Congrats! You guessed the number in ${this.prevGuesses}!`)
-  //     }else{
-  //       if(guess>this.secretNum){
-  //         alert(`Your guess is too high. Previous guesses`)
-  //       }else{
-  //       alert(`Your guess is too low. Previous guesses`)
-  //       }
-      // }
-  //   }
-  // }
-  
-
-
-
-game.play();
-// console.log(this.prevGuesses);
+// game.play();
